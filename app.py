4c78d0cec19acc017b1b90f9136744b397fd8d31
GaraziLS <glabakasanabria@gmail.com>
2024-08-14 19:26:03 +0200

    Started with login

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
